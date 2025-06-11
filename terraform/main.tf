7cb63623e8769ee713eafc8d7ef7af4104eaead0
Rafael Frassetto Pereira <rafafrass@gmail.com>
2025-06-11 10:18:55 -0300

    Configurando chave de acesso AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,9 @@ variable "docker_image" {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_ecs_cluster" "strapi_cluster" {
