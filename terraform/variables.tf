d2b9427f55fb213eeed6c7192b5bcce1120311f6
Rafael Frassetto Pereira <rafafrass@gmail.com>
2025-06-15 22:50:01 -0300

    fix : main.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
-
 variable "docker_image" {
   description = "The Docker image to deploy to ECS (e.g., rafafrassetto/html-app:latest)"
   type        = string
